7f567f65fa2528a08a8ee575203faa296123dc0b
DiptikaD <diptikadevi@gmail.com>
2025-02-06 16:43:52 -0500

    works locally!!

--- flask_app/views.py
+++ flask_app/views.py
@@ -2,10 +2,10 @@ from flask_app import app
 from flask_app.getProjects import getProjects
 from flask import jsonify
 
-#@app.route('/')
-#def index():
-#    return "Hello World!"
-
 @app.route('/')
+def index():
+   return "Hello World!"
+
+@app.route('/projects')
 def projects():
 	return jsonify(getProjects())
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask_app import app
 from flask_app import views
 from flask_app import getProjects
+    # even though views and getProjects are not used, they are required
 
 if __name__ == '__main__':
     app.run()
