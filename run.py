0b92e2b79bf47b9a804836ebfe828b32634380a5
DiptikaD <diptikadevi@gmail.com>
2025-02-06 16:36:20 -0500

    configured run

--- run.py
+++ run.py
@@ -1,2 +1,6 @@
-    # initial commit
-# app run and imports go here
+from flask_app import app
+from flask_app import views
+from flask_app import getProjects
+
+if __name__ == '__main__':
+    app.run()
