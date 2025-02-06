bb68d316255d8259d4a256d8deaed039567ead0b
DiptikaD <diptikadevi@gmail.com>
2025-02-06 17:03:16 -0500

    trouble shooting for domino instance

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,6 +1,10 @@
 from flask_app import app
 from flask_app.getProjects import getProjects
 from flask import jsonify
+from flask import Flask
+
+	# hardcoding the project id as the static url
+app = Flask(__name__, static_url_path='/modelproducts/67a52df95b902c4c38746378')
 
 @app.route('/')
 def index():
