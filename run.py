c6718200866f746f9745768e958c80364ba8fa3a
DiptikaD <diptikadevi@gmail.com>
2025-02-06 17:10:56 -0500

    Revert "works locally!!"
    This reverts commit 7f567f65fa2528a08a8ee575203faa296123dc0b.
    
    ok

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from flask_app import app
 from flask_app import views
 from flask_app import getProjects
-    # even though views and getProjects are not used, they are required
 
 if __name__ == '__main__':
     app.run()
