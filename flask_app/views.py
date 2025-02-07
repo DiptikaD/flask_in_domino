ea2a80c353ccde3ba3f0ce8c86d39f9a8f97c171
DiptikaD <diptikadevi@gmail.com>
2025-02-07 14:52:40 -0500

    testing html index

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,4 +1,5 @@
 from flask_app import app
+from flask import render_template
 from flask_app.getProjects import getProjects
 from flask import jsonify, send_from_directory
 import os
@@ -7,12 +8,13 @@ STATIC_FOLDER = os.path.join(os.path.dirname(__file__), 'templates')
 
 @app.route('/')
 def index():
-   return "Hello World!"
+   # return "Hello World!"
+   return render_template('index.html')
 
 @app.route('/projects')
 def projects():
 	return jsonify(getProjects())
 
-@app.route('/picture')
-def image():
-   return send_from_directory(STATIC_FOLDER, 'sheep_pose.jpg')
+# @app.route('/picture')
+# def image():
+#    return render_template('index.html')
